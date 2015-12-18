d6f2fea6965e27c34fb95d390a3e1c2169c42d79
Joseph Yan <josephyan123@yahoo.com>
2015-12-18 01:15:46 -0500

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,10 @@
+from __future__ import division
+import numpy as np
+import pandas as pd
+from pandas import Series, DataFrame
+import matplotlib.pyplot as plt
+from scipy import stats
+from statsmodels.formula.api import ols
+from statsmodels.stats.anova import anova_lm
+
+__all__ = ['mapping1', 'pcr1', 'pcr2', 'pwttest1', 'anova1', 'barplot1', 'boxplot1', 'barplot2']
