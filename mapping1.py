69036c53d2e762c60d354ce41266f135aa79e88d
Joseph Yan <josephyan123@yahoo.com>
2015-12-18 01:16:48 -0500

    Create mapping1.py

---
+++ mapping1.py
@@ -0,0 +1,5 @@
+
+mapping_1 = dict.fromkeys(['#001', '#002', '#003', '#004', '#005'], 'treat1 condi1')
+mapping_1.update(dict.fromkeys(['#006', '#007', '#008', '#009', '#010', '#011'], 'treat2 condi1'))
+mapping_1.update(dict.fromkeys(['#012', '#013', '#014', '#015', '#016', '#017', '#018', '#019', '#020', '#021'], 'treat1 condi2'))
+mapping_1.update(dict.fromkeys(['#022', '#023', '#024', '#025', '#026', '#027', '#028', '#029', '#030', '#031'], 'treat2 condi2'))
